d1d2a391ac47ec0add219ac25ed7926fcd8fea71
Henger <hengertg@gmail.com>
2026-06-30 10:38:32 -0400

    Preparing to update the wallet now with different classes as father class account

---
+++ Wallet V2.py
@@ -0,0 +1,27 @@
+class account:
+     pass
+
+
+class saving_account():
+     pass
+
+
+class checking_account():
+     pass
+
+
+class crypto_account():
+     pass
+
+
+class credit_account():
+     pass
+
+
+while True:
+
+    print("Welcome to your wallet, what do you want to do today?")
+
+    print("balance, history, deposit, transfer")
+
+    options = input("Choose an option: ")
