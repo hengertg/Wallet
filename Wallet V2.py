5ab7f5f6b90bb4469679cd07c1141d08fbb674e9
hengertg <hengertg@gmail.com>
2026-07-01 19:57:49 -0400

    Coding small stuff with the menu and adding to do list for things to finish.

--- Wallet V2.py
+++ Wallet V2.py
@@ -9,9 +9,11 @@ class Account:
      def deposit(self, amount):
           if amount <=0:
                return "Invalid balance, you can't deposit negative balance, please deposit the right amount"
+          
                         
           self.balance += amount
           self.history.append(f"{datetime.now()} - Deposit +$ {amount}")
+          return "You deposit has been successful"
 
      def get_balance(self):
           return self.balance
@@ -22,6 +24,20 @@ class Account:
                     
           self.balance -= amount
           self.history.append(f"{datetime.now()} - Tansfer -$ {amount}")
+
+
+
+# TO DO LIST FOR CLASS SAVING_ACCOUNT(ACCOUNT)
+
+# Deposit ✅
+
+# Withdraw ⬜
+
+# Transfer ⬜
+
+# History ⬜
+
+# Balance ⬜
 
 
 class Saving_account(Account):
@@ -35,6 +51,11 @@ class Saving_account(Account):
          fee = amount * 0.0015
          total = fee + amount
          print("Bank transfer with small fee")
+
+     def withdraw (self,amount):
+          self.withdraw -= amount
+          if self.withdraw < 0:
+               return "You can't withdraw 0 please, type right a amount to withdraw"
 
 
      
@@ -59,32 +80,60 @@ class Crypto_account(Account):
           fee = amount * 0.02
           total = amount + fee
           print("Crypto transfer with fee")
+          pass
 
 
 
-def make_transfer(account):
-     account.transfer()
+# def make_transfer(account):
+#     account.transfer()
 
 
-savingAccount = Saving_account("Henger", 0)
-checkingAccount = Checking_account()
-creditAccount = Credit_account()
-cryptoAccount = Crypto_account()
+# checkingAccount = Checking_account()
+# creditAccount = Credit_account()
+# cryptoAccount = Crypto_account()
 
 
-make_transfer(savingAccount)
-make_transfer(checkingAccount)
-make_transfer(creditAccount)
-make_transfer(cryptoAccount)
+# make_transfer(savingAccount)
 
 
+# TO DO LIST FOR MENU
+
+# Principal Menu ✅
+
+# Saving Account Menu⬜
+
+# Checking Account Menu ⬜
+
+# Credit Account Menu ⬜
+
+# Crypto Account Menut ⬜
 
 while True:
 
      print("\nWelcome to your wallet, what do you want to do today?")
-     print("balance, history, deposit, transfer")
+     print("Type the account you want to choose")
+     print("Saving Account, Checking Account, Credit Account, Crypto Account, Go Back: to go to previous menu")
 
-     options = input("Choose an option: ")
+     user_options = input("Choose an option: ")
 
-     if options == "deposit":
-          print(f"You Deposited {savingaccount.deposit(0)}")
+
+
+     if user_options == "Saving Account":
+          savingAccount = Saving_account("Henger", 0)
+          while True:
+
+               print ("What do you want to do?")
+               print("Deposit, Transfer, Withdraw, history or if you want to go back to previus menu pleease type Go back")
+               saving_options = input("Type an option: ")
+
+               if saving_options == "Deposit":
+
+                    deposit_amount= int(input("Please type how much you want to deposit: "))
+                    print(f"You Deposited {savingAccount.deposit(deposit_amount)}")
+
+               if saving_options == "Go back":
+                    break
+                         
+               
+     elif user_options == "Exit":
+          break
